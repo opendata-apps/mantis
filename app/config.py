5f956dff202c02d7e9f60168d9c52a37e07dd0f1
LeonKohli <kohli.leon@gmail.com>
2023-05-09 13:55:17 +0200

    migration database

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://mantis_user:mantis@localhost/mantis_tracker'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/mantis_tracker'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAP_CENTER_LONGITUDE = -122.4194
     MAP_CENTER_LATITUDE = 37.7749
