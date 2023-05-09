cda7411d19e748735f4c1af83855bf1925a090e9
LeonKohli <kohli.leon@gmail.com>
2023-05-09 13:31:07 +0200

    z index navbar overlap

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class ReportForm(FlaskForm):
     latitude = FloatField('Latitude', validators=[DataRequired()])
     longitude = FloatField('Longitude', validators=[DataRequired()])
