93ad17f6c2d9f8595cdb7fb91d71e23d09f9873d
PeterK <peter@koppatz.com>
2023-05-18 12:34:44 +0200

    DB-Struktur erweitert; Demodaten Beschreibung auf einfache Liste reduziert

--- app/database/fundmeldungen.py
+++ app/database/fundmeldungen.py
@@ -6,15 +6,20 @@ class TblMeldungen(db.Model):
     __tablename__ = "meldungen"
 
     id = db.Column(db.Integer, primary_key=True)
-    dat_fund = db.Column(db.Date, nullable=False)
-    dat_meld = db.Column(db.Date, nullable=False)
+    dat_fund_von = db.Column(db.Date, nullable=False)
+    dat_fund_bis = db.Column(db.Date, nullable=True)
+    dat_meld = db.Column(db.Date, nullable=True)
     dat_bear = db.Column(db.Date, nullable=True)
     anzahl = db.Column(db.Integer, nullable=True)
+    art_m = db.Column(db.String(1), nullable=True)
+    art_w = db.Column(db.String(1), nullable=True)
+    art_n = db.Column(db.String(1), nullable=True)
+    art_o = db.Column(db.String(1), nullable=True)    
     fo_zuordnung = db.Column(
         db.Integer,  db.ForeignKey("fundorte.id"), nullable=True)
     fo_quelle = db.Column(db.String(1), nullable=True)
-    fo_kategorie = db.Column(db.String(1), nullable=True)
-    anmerkung = db.Column(db.String(500), nullable=True)
+    anm_melder = db.Column(db.String(500), nullable=True)
+    anm_bearbeiter = db.Column(db.String(500), nullable=True)
 
     def __repr__(self):
         return f'<Report {self.id}>'
@@ -22,12 +27,17 @@ class TblMeldungen(db.Model):
     def to_dict(self):
         return {
             'id': self.id,
-            'dat_fund': self.dat_fund,
+            'dat_fund_von': self.dat_fund,
+            'dat_fund_bis': self.dat_fund,
             'dat_meld': self.dat_meld,
             'dat_bear': self.dat_bear,
             'anzahl': self.anzahl,
+            'art_m': self.art_m,
+            'art_w': self.art_m,
+            'art_n': self.art_m,
+            'art_0': self.art_m,
             'fo_zuordung': self.fo_zuordnung,
             'fo_quelle': self.fo_quelle,
-            'fo_kategorie': self.fo_kategorie,
-            'anmerkung': self.anmerkung,
+            'anm_melder': self.anmerkung,
+            'anm_bearbeiter': self.anmerkung,
         }
--- app/database/fundorte.py
+++ app/database/fundorte.py
@@ -9,12 +9,16 @@ class TblFundorte (db.Model):
     plz = db.Column(db.Integer, nullable=False)
     ort = db.Column(db.String, nullable=False)
     strasse = db.Column(db.String(100), nullable=False)
-    land = db.Column(db.String(50), nullable=False)
     kreis = db.Column(db.String, nullable=False)
+    land = db.Column(db.String(50), nullable=False)
+    amt = db.Column(db.String(50), nullable=False)
+    mtb = db.Column(db.String(50), nullable=False)
     beschreibung = db.Column(db.Integer, db.ForeignKey(
         "beschreibung.id"), nullable=False)
     longitude = db.Column(db.VARCHAR(25), nullable=False)
     latitude = db.Column(db.VARCHAR(25), nullable=False)
+    beschreibung = db.Column(db.Integer, db.ForeignKey(
+        "beschreibung.id"), nullable=False)
     ablage = db.Column(db.VARCHAR(255), nullable=False)
 
     def __repr__(self):
@@ -26,9 +30,12 @@ class TblFundorte (db.Model):
             'plz': self.plz,
             'ort': self.ort,
             'strasse': self.strasse,
-            'land': self.land,
             'kreis': self.kreis,
-            'beschreibung': self.beschreibung,
+            'land': self.land,
+            'amt': self.amt,
+            'mtb': self.mtb,
             'latitude': self.latitude,
             'longitude': self.longitude,
+            'beschreibung': self.beschreibung,
+            'ablage': self.ablage,
         }
