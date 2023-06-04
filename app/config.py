3f1135ffab7a5c69332716df566173033b8975ff
PeterK <peter@koppatz.com>
2023-06-04 15:20:12 +0200

    Upload der Bilder vorbereitet: Ordner definiert

--- app/config.py
+++ app/config.py
@@ -2,12 +2,13 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://mantis_user:mantis@localhost/mantis_tracker'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/mantis_tracker'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAP_CENTER_LONGITUDE = -122.4194
     MAP_CENTER_LATITUDE = 37.7749
     MAP_ZOOM = 1
     SECRET_KEY = os.urandom(24)
+    UPLOAD_PATH = "datastore"
     host = ""
     port = 25
     tls = True
