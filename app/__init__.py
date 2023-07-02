beb0e47bad1858556e2cf6fdaf731787a3da03cf
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2023-07-02 23:06:19 +0200

    except admin main review

--- app/__init__.py
+++ app/__init__.py
@@ -16,15 +16,21 @@ def create_app(config_class=Config):
     csrf.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
-    # If using Flask-App behind Nginx                                                                                                                        
-    # https://flask.palletsprojects.com/en/2.3.x/deploying/proxy_fix/                                                                                        
+    # If using Flask-App behind Nginx
+    # https://flask.palletsprojects.com/en/2.3.x/deploying/proxy_fix/
     app.wsgi_app = ProxyFix(
         app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
     )
+
     from app.routes.main import main
     from app.routes.admin import admin
     from app.routes.data import data
     from app.routes.reviewer import review
+
+    csrf.exempt(main)
+    csrf.exempt(admin)
+    csrf.exempt(review)
+
     app.register_blueprint(main)
     app.register_blueprint(admin)
     app.register_blueprint(data)
