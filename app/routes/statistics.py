281c31e734d302d350c90465f874593473b464a9
PeterK <peter@koppatz.com>
2023-10-08 09:58:32 +0200

    Erste Auswertung nach Art mit Dummy-Daten

--- app/routes/statistics.py
+++ app/routes/statistics.py
@@ -1,4 +1,5 @@
 from flask import jsonify, render_template, request, Blueprint, send_from_directory
+import json
 
 stats = Blueprint("statstics", __name__)
 
@@ -15,3 +16,15 @@ def stats_start():
     "Startseite für alle Statistiken"
 
     return render_template("statistics/statistiken.html", menu=list_of_stats)
+
+@stats.route("/statistik/geschlecht")
+def stats_geschlecht():
+    "Count sum of all kategories"
+
+    rows = {'x': ['Ootheken', 'Nymphen','Weibchen', 'Männchen', 'Andere','Summe'],
+            'y': [10, 1000, 1100, 2000, 20, 4140]
+    }
+
+    return render_template("statistics/stats-geschlecht.html",
+                           menu=list_of_stats,
+                           values=rows)
