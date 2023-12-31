171416ecf3d83af0838c811bc4e7114803554c4e
PeterK <peter@koppatz.com>
2023-12-31 21:27:11 +0100

    Verknüpfung des erweiterten Revieer-Menüs
    flake8

---
+++ app/tools/check_reviewer.py
@@ -0,0 +1,10 @@
+from functools import wraps
+from flask import session  # import session
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
