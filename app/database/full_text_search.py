f4a0d6d5535ee38d7e3f38993e279e5109a70e17
PeterK <peter@koppatz.com>
2024-02-20 13:32:46 +0100

    fix: #110 Anlage Materialzed View ohne gleichnamige Tabelle

--- app/database/full_text_search.py
+++ app/database/full_text_search.py
@@ -1,12 +1,11 @@
 from app import db
 from sqlalchemy import text
 from sqlalchemy.dialects.postgresql import TSVECTOR
-
-
-class FullTextSearch(db.Model):
-    __tablename__ = 'full_text_search'
-    meldungen_id = db.Column(db.Integer, primary_key=True)
-    doc = db.Column(TSVECTOR)
+    
+class FullTextSearch():
+#    __tablename__ = 'full_text_dummy'
+#    meldungen_id = db.Column(db.Integer, primary_key=True)
+#    doc = db.Column(TSVECTOR)
 
     @staticmethod
     def create_materialized_view():
