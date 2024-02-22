fff5001ca2801abc12ecef789ea6efa6cdd4fd1c
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2024-02-22 23:40:11 +0100

    chore small changes to fts, fixed broken search

--- app/database/full_text_search.py
+++ app/database/full_text_search.py
@@ -1,8 +1,12 @@
 from app import db
 from sqlalchemy import text
+from sqlalchemy.dialects.postgresql import TSVECTOR
 
 
-class FullTextSearch:
+class FullTextSearch(db.Model):
+    __tablename__ = "full_text_search"
+    meldungen_id = db.Column(db.Integer, primary_key=True)
+    doc = db.Column(TSVECTOR)
 
     @staticmethod
     def create_materialized_view():
