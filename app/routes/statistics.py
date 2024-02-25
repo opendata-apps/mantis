733ba5790db68e4e122fc0841bdbc4b698f10986
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2024-02-25 21:33:48 +0100

    chore: typo

--- app/routes/statistics.py
+++ app/routes/statistics.py
@@ -14,7 +14,7 @@ list_of_stats = {
     "start": "Statistik: Startseite",
     "geschlecht": "Entwicklungsstadium/Geschlecht",
     "meldungen_funddatum": "Meldungen: Funddatum",
-    "meldungen_meldedatum": "Meldungen: Meledatum",
+    "meldungen_meldedatum": "Meldungen: Meldedatum",
     "meldungen_meld_fund": "Meldungen: Fund- und Meldedatum",
 }
 
@@ -24,14 +24,14 @@ list_of_stats = {
 @login_required
 def stats_start(usrid=None):
     "Startseite für alle Statistiken"
-    user_id = session['user_id']
+    user_id = session["user_id"]
     user = TblUsers.query.filter_by(user_id=user_id).first()
 
     # If the user doesn't exist or the role isn't 9, return 404
-    if not user or user.user_rolle != '9':
+    if not user or user.user_rolle != "9":
         abort(403)
 
-    value = request.form.get('stats', 'start')
+    value = request.form.get("stats", "start")
     if value == "geschlecht":
         return stats_geschlecht()
     elif value == "meldungen_meldedatum":
@@ -41,21 +41,26 @@ def stats_start(usrid=None):
     elif value == "meldungen_meld_fund":
         return stats_meldungen_meld_fund()
     elif value == "start":
-        return render_template("statistics/statistiken.html",
-                               user_id=session['user_id'],
-                               menu=list_of_stats,
-                               marker="start")
+        return render_template(
+            "statistics/statistiken.html",
+            user_id=session["user_id"],
+            menu=list_of_stats,
+            marker="start",
+        )
     else:
-        return render_template("statistics/statistiken.html",
-                               user_id=session['user_id'],
-                               menu=list_of_stats,
-                               marker="start")
+        return render_template(
+            "statistics/statistiken.html",
+            user_id=session["user_id"],
+            menu=list_of_stats,
+            marker="start",
+        )
 
 
 def stats_geschlecht(request=None):
     "Count sum of all kategories"
 
-    sql = text('''
+    sql = text(
+        """
       select sum(art_m) as "Männchen"
             , sum(art_w) as "Weibchen"
             , sum(art_n) as "Nymphen"
@@ -63,7 +68,8 @@ def stats_geschlecht(request=None):
             , sum(art_f) as "Andere"
       from meldungen
       where deleted is NULL;
-    ''')
+    """
+    )
 
     with db.engine.connect() as conn:
         result = conn.execute(sql)
@@ -72,118 +78,129 @@ def stats_geschlecht(request=None):
             row = row._mapping
             res = dict((name, val) for name, val in row.items())
 
-    return render_template("statistics/stats-geschlecht.html",
-                           menu=list_of_stats,
-                           user_id=session['user_id'],
-                           marker="geschlecht",
-                           values=res)
+    return render_template(
+        "statistics/stats-geschlecht.html",
+        menu=list_of_stats,
+        user_id=session["user_id"],
+        marker="geschlecht",
+        values=res,
+    )
 
 
 def stats_meldungen_meldedatum(request=None):
     "Cluster messages per dat_meld"
 
-    sql = text(""" SELECT dat_meld as Tag,
+    sql = text(
+        """ SELECT dat_meld as Tag,
                    count(dat_meld) as Anzahl
                    FROM meldungen
                    GROUP BY Tag
-                   ORDER by Tag;""")
+                   ORDER by Tag;"""
+    )
     with db.engine.connect() as conn:
         result = conn.execute(sql)
 
-    trace1 = {"x": [],
-              "y": []}
+    trace1 = {"x": [], "y": []}
 
     if result:
         for record in result:
-            trace1['x'].append(str(record[0]))
-            trace1['y'].append(record[1])
+            trace1["x"].append(str(record[0]))
+            trace1["y"].append(record[1])
         else:
             pass
     res = json.loads(json.dumps(trace1))
 
-    return render_template("statistics/stats-meldedatum.html",
-                           menu=list_of_stats,
-                           marker="medlungen_meldedatum",
-                           user_id=session['user_id'],
-                           values=res)
+    return render_template(
+        "statistics/stats-meldedatum.html",
+        menu=list_of_stats,
+        marker="medlungen_meldedatum",
+        user_id=session["user_id"],
+        values=res,
+    )
 
 
 def stats_meldungen_funddatum(request=None):
     "Cluster messages per dat_fund"
 
-    sql = text(""" SELECT dat_fund_von as Tag,
+    sql = text(
+        """ SELECT dat_fund_von as Tag,
                    count(dat_fund_von) as Anzahl
                    FROM meldungen
                    GROUP BY Tag
-                   ORDER by Tag;""")
+                   ORDER by Tag;"""
+    )
     with db.engine.connect() as conn:
         result = conn.execute(sql)
 
-    trace1 = {"x": [],
-              "y": []}
+    trace1 = {"x": [], "y": []}
 
     if result:
         for record in result:
-            trace1['x'].append(str(record[0]))
-            trace1['y'].append(record[1])
+            trace1["x"].append(str(record[0]))
+            trace1["y"].append(record[1])
         else:
             pass
     res = json.loads(json.dumps(trace1))
 
-    return render_template("statistics/stats-funddatum.html",
-                           menu=list_of_stats,
-                           marker="meldungen_funddatum",
-                           user_id=session['user_id'],
-                           values=res)
+    return render_template(
+        "statistics/stats-funddatum.html",
+        menu=list_of_stats,
+        marker="meldungen_funddatum",
+        user_id=session["user_id"],
+        values=res,
+    )
 
 
 def stats_meldungen_meld_fund(request=None):
     "Cluster messages per day"
 
-#    dbsession = db.engine.connect()
-    sql = text(""" SELECT dat_meld as Tag,
+    #    dbsession = db.engine.connect()
+    sql = text(
+        """ SELECT dat_meld as Tag,
                    count(dat_meld) as Anzahl
                    FROM meldungen
                    GROUP BY Tag
-                   ORDER by Tag;""")
+                   ORDER by Tag;"""
+    )
     with db.engine.connect() as conn:
         result = conn.execute(sql)
 
-    trace1 = {"x": [],
-              "y": []}
+    trace1 = {"x": [], "y": []}
 
     if result:
         for record in result:
-            trace1['x'].append(str(record[0]))
-            trace1['y'].append(record[1])
+            trace1["x"].append(str(record[0]))
+            trace1["y"].append(record[1])
         else:
             pass
     res_trace1 = json.loads(json.dumps(trace1))
 
-    sql = text(""" SELECT dat_fund_von as Tag,
+    sql = text(
+        """ SELECT dat_fund_von as Tag,
                    count(dat_fund_von) as Anzahl
                    FROM meldungen
                    GROUP BY Tag
-                   ORDER by Tag;""")
+                   ORDER by Tag;"""
+    )
     with db.engine.connect() as conn:
         result = conn.execute(sql)
 
-    trace2 = {"x": [],
-              "y": []}
+    trace2 = {"x": [], "y": []}
 
     if result:
         for record in result:
-            trace2['x'].append(str(record[0]))
-            trace2['y'].append(record[1])
+            trace2["x"].append(str(record[0]))
+            trace2["y"].append(record[1])
         else:
             pass
     res_trace2 = json.loads(json.dumps(trace2))
     print(res_trace1)
     print(res_trace2)
-    return render_template("statistics/stats-meld-fund.html",
-                           menu=list_of_stats,
-                           marker="meldungen_meld_fund",
-                           user_id=session['user_id'],
-                           trace1=res_trace1,
-                           trace2=res_trace2
-                           )
+    return render_template(
+        "statistics/stats-meld-fund.html",
+        menu=list_of_stats,
+        marker="meldungen_meld_fund",
+        user_id=session["user_id"],
+        trace1=res_trace1,
+        trace2=res_trace2,
+    )
