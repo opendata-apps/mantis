2aae69ca74a9057593fed96691c5b21a755f4ff2
PeterK <peter@koppatz.com>
2024-06-15 14:12:55 +0200

    Setup für aemter-Tabelle vorbereitet

--- app/database/aemter_koordinaten.py
+++ app/database/aemter_koordinaten.py
@@ -4,14 +4,14 @@ Koordinaten ein Amt zuzuordnen.
 """
 
 from app import db
-
+from sqlalchemy.dialects.postgresql import JSONB
 
 class TblAemterCoordinaten(db.Model):
     __tablename__ = "aemter"
 
     ags = db.Column(db.Integer, primary_key=True)
     gen = db.Column(db.String(100), nullable=False)
-    properties = db.Column(db.json, nullable=False)
+    properties = db.Column(JSONB, nullable=False)
 
     def __repr__(self):
         return f"<Report {self.ags}>"
