fda6c81ad4ff3de03f15b3e7bb16c391813e3c37
PeterK <peter@koppatz.com>
2024-06-16 15:56:15 +0200

    new: Auswertung nach Gemeinde/Amt

--- app/tools/find_gemeinde.py
+++ app/tools/find_gemeinde.py
@@ -24,6 +24,10 @@ def get_amt_full_scan(point):
 
 if __name__ == '__main__':
 
+    SQLALCHEMY_DATABASE_URI = 'postgresql://mantis_user:mantis@localhost/mantis_tracker'
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    
     point = [12.07906, 51.3324]
     point = [10.895, 48.3745]
+
     print(get_amt_full_scan(point))
