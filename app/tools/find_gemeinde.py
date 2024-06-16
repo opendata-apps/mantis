d35df1ee9570b832893549a4801795d2e5e1b5f8
PeterK <peter@koppatz.com>
2024-06-16 12:11:08 +0200

    DB-Verbindung aus Config.py ausgelesen

--- app/tools/find_gemeinde.py
+++ app/tools/find_gemeinde.py
@@ -1,31 +1,25 @@
 import json
-import psycopg2
 from shapely.geometry import shape, Point
+from app.config import Config
+from sqlalchemy import create_engine
+from sqlalchemy import text
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 
 
 def get_amt_full_scan(point):
-    conn = psycopg2.connect(
-        dbname="mantis_tracker",
-        user="mantis_user",
-        password="mantis",
-        host="localhost"
-    )
-    cur = conn.cursor()
-
-    stm = "select * from aemter"
-    cur.execute(stm)
-    rows = cur.fetchall()
-    for row in rows:
-        coords = json.loads(str(row[2]).replace("'", '"'))
-        polygon = shape(coords)
-        point = Point(point)
-        inside = polygon.contains(point)
-        if inside:
-            ags = row[0]
-            gem = row[1]
-            cur.close()
-            conn.close()
-            return f"{ags} -- {gem}"
+    with engine.connect() as conn:
+        stm = "select * from aemter"
+        rows = conn.execute(text(stm))
+        for row in rows:
+            coords = json.loads(str(row[2]).replace("'", '"'))
+            polygon = shape(coords)
+            point = Point(point)
+            inside = polygon.contains(point)
+            if inside:
+                ags = row[0]
+                gem = row[1]
+                return f"{ags} -- {gem}"
 
 
 if __name__ == '__main__':
