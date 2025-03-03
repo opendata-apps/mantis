b32a173b5d75f7006c04479e796074d2a1d7a66a
PeterK <gitlab@koppatz.com>
2025-03-03 20:54:42 +0100

    First test for Messtischblatt.

---
+++ tests/statistics/test_get_messtischblatt.py
@@ -0,0 +1,8 @@
+from app.tools.mtb_calc import get_mtb, pointInRect
+
+def test_get_mtb_for_trebbin(session):
+
+    point = [13.440228 , 51.738052]
+    mtb = get_mtb(point[1], point[0])
+
+    assert mtb == "4246"
