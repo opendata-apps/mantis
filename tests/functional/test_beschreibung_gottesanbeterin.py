e1430e98a245cf2b0e2bfe272b277b7df77aaa6b
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2025-03-09 20:48:06 +0100

    test: Update Mantis Religiosa page text in functional test

--- tests/functional/test_beschreibung_gottesanbeterin.py
+++ tests/functional/test_beschreibung_gottesanbeterin.py
@@ -1,4 +1,4 @@
 def test_request_example(client):
     response = client.get("/mantis-religiosa")
-    text = bytes("Die faszinierende Welt der", "utf-8")
+    text = bytes("Die Europäische Gottesanbeterin", "utf-8")
     assert text in response.data
