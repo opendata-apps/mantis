f3384f6ee39d24400c4fc0e20ea933b1d432a70a
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2025-11-16 21:35:03 +0100

    refactor: update CELEBRATION_THRESHOLD to use environment variable from .env.example

--- app/config.py
+++ app/config.py
@@ -75,7 +75,7 @@ class Config:
     # Application Settings
     STATIC_FOLDER = 'app/static'
     CURRENT_YEAR = datetime.now().year
-    CELEBRATION_THRESHOLD = 10000
+    CELEBRATION_THRESHOLD = os.getenv('CELEBRATION_THRESHOLD')
     TESTING = os.getenv('TESTING', 'False').lower() in ('true', '1', 'yes')
     CHECKLIST = {}
  
